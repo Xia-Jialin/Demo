ccaf3dee776534c2d6b711db58634c0b7f082e8a
夏佳林 <xiajialin@xiajialindeMacBook-Pro.local>
2020-10-30 23:39:12 +0800

    初始化

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func init() {
+	file := "./" + "message" + ".log"
+	logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+	if err != nil {
+		panic(err)
+	}
+	log.SetOutput(logFile) // 将文件设置为log输出的文件
+	log.SetPrefix("[qSkipTool]")
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
+	return
+}
+func main() {
+	fmt.Println("Hello Worlsd")
+	log.Println("123")
+}
